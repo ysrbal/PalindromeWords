287228ae38977bed627cbcef1f12376cdb14556b
yasar.bal <ysr.bal@gmail.com>
2022-07-24 00:54:43 +0300

    first commit

---
+++ PalindromeWords/src/Words.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Words {
+    static boolean isPalindrome(String str) {
+        int i = 0;
+        int j = str.length() - 1;
+        while (i < j) {
+            if (str.charAt(i) != str.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Lutfen Bir Kelime Giriniz : ");
+        String str = inp.nextLine();
+        if (isPalindrome(str)){
+            System.out.println("Kelime Palindrom kelimedir.");
+        }else {
+            System.out.println("Kelime Palindrom kelime degildir.");
+        }
+    }
+}
